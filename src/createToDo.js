575263f554d7989ab159ac97786f3981bab1513e
melinabed <melo36049@hotmail.com>
2023-11-10 11:30:03 -1000

    added logic to add button with all info

--- src/createToDo.js
+++ src/createToDo.js
@@ -1,15 +1,50 @@
+import { compareAsc, format, parseISO, startOfToday } from 'date-fns';
+import { clearForm } from './domManip';
+
 //Creating an array for the ToDo
 let toDoArray = [];
 
-export const createToDo = (title, description, dueDate, priority, listType) => {
-    console.log('creating to do now..');
-    console.log({title, description, dueDate, priority, listType});
-    console.log('pushing this object to the todoarray...');
+export const createToDo = () => {
+    let title = document.getElementById('title').value;
+    let listType = document.getElementById('list-type').value;
+    let dueDate = document.getElementById('duedate').value;
+    let priority = document.getElementById('priority').value;
 
-    //TODO: remove lines code if not needed
+    //Check to see if empty fields exist
+    if (title == "" || dueDate == "") {
+        alert('Title, notes, and due date are required fields, please try again')
+        return;
+    }
+    //Check to see if pre-date was entered
+    if (parseISO(dueDate) < startOfToday()) {
+        alert('you have entered a date that has already passed! please enter a date greater than that.')
+        console.log('due date', parseISO(dueDate));
+        console.log('date now', startOfToday());
+        return;
+    }
+    //Loop over the nodelist for notes list items from the DOM and format to string
+    const nodeListNotes = document.querySelectorAll('li');
+    let notesListArray = [];
+    for (let i = 0; i < nodeListNotes.length; i++) {
 
-    toDoArray.push({title, description, dueDate, priority, listType});
+        //Strip off the "x" from the end of the li then push to temp array
+        let strippedNotesList = nodeListNotes[i].textContent.replace('\u00D7', "");
+        notesListArray.push(strippedNotesList);
+    }
+    //Strip off the notes list array and convert to string with commas
+    let notes = notesListArray.join(", ");
+
+    console.log('called createToDO module.. creating todo now');
+    console.log({title, notes, dueDate, priority, listType });
+    console.log('pushing this object to the toDo array...');
+    //TODO: remove below two lines if array is not needed
+    toDoArray.push({title, notes, dueDate, priority, listType});
     console.log(toDoArray);
-    return {title, description, dueDate, priority, listType};
-    
+
+    //Reset the form after successful submission
+    clearForm();
+
+    return {title, notes, dueDate, priority, listType};
+
 }
+    
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { projectLoad } from "./projectLoad";
 import { displayDefaultProject, displayForm, addNotesList, clearForm } from "./domManip";
 import { createToDo } from "./createToDo";
 
+
 //Call the project load on first land
 projectLoad();
 
@@ -22,9 +23,13 @@ let clickEventModule = (function() {
     //Click even to clear the form
     const clearButton = document.querySelector('.reset-button');
     clearButton.addEventListener('click', clearForm);
+
+    //Click event to submit a new todo form to project
+    const submitButton = document.querySelector('.submit-button');
+    submitButton.addEventListener('click', createToDo);
 })();
 
-const myToDo = createToDo("Grocery Run", "go get the groceries", "6/11/2023", "Low", "Grocery List");
-const myToDo2 = createToDo("Homework", "Biology", "11/17/2023", "Medium", "Reminders");
-console.log("show properties from index.js", myToDo);
-console.log("show properties from index.js", myToDo2);
+//const myToDo = createToDo("Grocery Run", "go get the groceries", "6/11/2023", "Low", "Grocery List");
+//const myToDo2 = createToDo("Homework", "Biology", "11/17/2023", "Medium", "Reminders");
+//console.log("show properties from index.js", myToDo);
+//console.log("show properties from index.js", myToDo2);
